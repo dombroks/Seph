06060571d9af8ac5c0ccabaebc5c9cede994dd47
Dom Broks <tinoutech@gmail.com>
2026-02-05 17:46:35 +0100

    Optimize imports

--- app/src/main/java/com/younesbelouche/seph/features/products/presentation/ProductsScreen.kt
+++ app/src/main/java/com/younesbelouche/seph/features/products/presentation/ProductsScreen.kt
@@ -1,23 +1,13 @@
 package com.younesbelouche.seph.features.products.presentation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.younesbelouche.seph.features.products.presentation.components.ProductsList
-import com.younesbelouche.seph.features.products.presentation.components.ProductsSearchBar
 import com.younesbelouche.seph.features.products.presentation.components.ProductsTopBar
 import com.younesbelouche.seph.features.products.presentation.models.ProductReviewsUi
 import com.younesbelouche.seph.features.products.presentation.models.ProductUi
