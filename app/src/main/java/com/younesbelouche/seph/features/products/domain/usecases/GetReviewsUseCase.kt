4b941ccf56fc5b9910ee605ff523d1a0cc952b94
Dom Broks <tinoutech@gmail.com>
2026-02-06 14:41:06 +0100

    Optimize imports

--- app/src/main/java/com/younesbelouche/seph/features/products/domain/usecases/GetReviewsUseCase.kt
+++ app/src/main/java/com/younesbelouche/seph/features/products/domain/usecases/GetReviewsUseCase.kt
@@ -1,9 +1,7 @@
 package com.younesbelouche.seph.features.products.domain.usecases
 
 import com.younesbelouche.seph.core.util.Result
-import com.younesbelouche.seph.features.products.domain.entities.Product
 import com.younesbelouche.seph.features.products.domain.entities.ProductReviews
-import com.younesbelouche.seph.features.products.domain.entities.Review
 import com.younesbelouche.seph.features.products.domain.repositories.ProductsRepository
 import kotlinx.coroutines.flow.Flow
 
